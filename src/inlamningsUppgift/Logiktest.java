c90d405dcea10e04eea6302f9f05870217ee7e54
jama_ <jama_zamzam@hotmail.com>
2025-06-10 21:36:39 +0200

    complete

--- src/inlamningsUppgift/Logiktest.java
+++ src/inlamningsUppgift/Logiktest.java
@@ -2,50 +2,55 @@ package inlamningsUppgift;
 
 import org.junit.Test;
 
-import wordCounter.WordCounter;
 
 import static org.junit.Assert.assertEquals;
 
 public class Logiktest {
 
+
     @Test
-    public void getInitialNumberOfSavedRows(){
+    public void getteckenCount() {
 
+        Logik logik = new Logik();
+
+        int actual = logik.teckenCount("Zamzam");
+        int expected = 6;
         //assertEquals(expected, actual);
-        int expected = 1;
-
-      Logik logik = new Logik();
-
-      // int actual =logik.getRows();
-
-      // assertEquals(expected, actual);
+        assertEquals(expected, actual);
 
     }
+
     @Test
-    public void setOneRow(){
-
-       /* WordCounter counter = new WordCounter();
-       counter.checkText("Hej jag heter Staffan");
-
-       //expected, Actual
-
-        int actual= counter.getRows();
-        int expected = 1;
+    public void getlongestWord() {
+        Logik logik = new Logik();
+        String actual = logik.longestWord("my name is Zamzam");
+        String expected = "Zamzam";
+        //String expected = "Zamzam";
         assertEquals(expected, actual);
     }
 
 
     @Test
-    public void setTwoRow(){
+    public void setOneRow() {
+        Logik logik = new Logik();
+        logik.checkText("Hej jag heter Staffan");
+        // logik.checkText("Vilken fin och soligt dag det är");
+        int actual = logik.getRows();
+        int expected = 1;
+        assertEquals(expected, actual);
 
-        WordCounter counter = new WordCounter();
-        counter.checkText("Hej jag heter Staffan");
-        counter.checkText("Vilken fin och soligt dag det är");
 
-        //expected, Actual
+    }
 
-        int actual= counter.getRows();
+
+    @Test
+    public void setTwoRow() {
+
+        Logik logik = new Logik();
+        logik.checkText("Hej jag heter Staffan");
+        logik.checkText("Vilken fin och soligt dag det är");
+        int actual = logik.getRows();
         int expected = 2;
         assertEquals(expected, actual);
-    }*/
-}}
+    }
+}
